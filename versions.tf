638ca1f2ca9f3cbc2667f53ff48075d0bbe03a58
FabrizioCafolla <developer@fabriziocafolla.com>
2024-07-23 14:28:27 +0200

    feat: update providers

--- versions.tf
+++ versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.51.0, < 5.0, !=4.65.0, !=4.65.1"
+      version = ">= 5.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.51.0, < 5.0, !=4.65.0, !=4.65.1"
+      version = ">= 5.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
